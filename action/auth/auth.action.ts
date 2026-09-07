19b1dc0da7c2007f32f52f561ee3374289e967f4
newbiejy235 <dbankaii1@gmail.com>
2026-09-07 17:18:49 +0700

    add feat: register

--- action/auth/auth.action.ts
+++ action/auth/auth.action.ts
@@ -2,6 +2,7 @@
 
 import { cookies } from "next/headers";
 import { authService } from "@/lib/auth/auth.service";
+import { getAuthenticatedUser } from "@/lib/auth/getUser";
 
 export async function loginAction(
   email: string,
@@ -31,4 +32,19 @@ export async function loginAction(
     message: "Login berhasil",
     user: result.user,
   };
+}
+
+export async function registerAction(username : string, email : string, password : string) {
+    if (!username || !email || !password) {
+        return({
+            success : false,
+            message : "silahkan isi semua form nya"
+        })
+    }
+
+    return await authService.register(username, email, password)
+}
+
+export async function getUserLogin() {
+    return await getAuthenticatedUser()
 }
