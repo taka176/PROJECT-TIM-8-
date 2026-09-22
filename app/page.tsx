da814b2102aa51a6bb15863030edc2fce927f477
Qwerty123aGe <augiearrafi6@gmail.com>
2026-09-23 00:22:51 +0700

    feat: update folder app dan components slicing y

---
+++ app/page.tsx
@@ -0,0 +1,44 @@
+// src/app/page.tsx
+// import Loginril from "../Components/Loginril";
+
+// export default function Home() {
+//   return (
+//     <main className="min-h-screen flex items-center justify-center p-4">
+//       <Loginril />
+//     </main>
+//   );
+// }
+
+"use client";
+
+import { useState } from "react";
+import Loginril from "../Components/Loginril";
+import Register from "../Components/Register";
+import TodokartUtama from "../Components/TodokartUtama";
+
+export default function Home() {
+  // state kontrol halaman: 'login' | 'register' | 'todo'
+  const [currentView, setCurrentView] = useState<"login" | "register" | "todo">(
+    "login",
+  );
+
+  return (
+    <main>
+      {currentView === "login" && (
+        <Loginril
+          onNavigateToRegister={() => setCurrentView("register")}
+          onSubmit={() => {
+            setCurrentView("todo");
+            return false;
+          }}
+        />
+      )}
+
+      {currentView === "register" && (
+        <Register onNavigateToLogin={() => setCurrentView("login")} />
+      )}
+
+      {currentView === "todo" && <TodokartUtama />}
+    </main>
+  );
+}
